b46d2c3b1cc69523de218989ce1ff4160381a295
Ryanalys <ryanalys@grinnell.edu>
2024-12-07 17:10:31 -0600

    Implemented set and get methods

--- src/main/java/edu/grinnell/csc207/util/BitTree.java
+++ src/main/java/edu/grinnell/csc207/util/BitTree.java
@@ -7,13 +7,17 @@ import java.io.PrintWriter;
  * Trees intended to be used in storing mappings between fixed-length 
  * sequences of bits and corresponding values.
  *
- * @author Your Name Here
+ * @author Alyssa Ryan
  */
 public class BitTree {
   // +--------+------------------------------------------------------
   // | Fields |
   // +--------+
-
+  /**
+   * The length of the string that will be given
+   */
+  int stringSize;
+  BitTreeNode root;
   // +--------------+------------------------------------------------
   // | Constructors |
   // +--------------+
@@ -22,6 +26,8 @@ public class BitTree {
    *
    */
   public BitTree(int n) {
+    this.stringSize = n;
+    this.root = new BitTreeNode();
   } // BitTree(int)
 
   // +---------------+-----------------------------------------------
@@ -36,14 +42,57 @@ public class BitTree {
    *
    */
   public void set(String bits, String value) {
-    // STUB
+    if(bits.length() != stringSize) {
+      throw new IndexOutOfBoundsException();
+    } ///if
+
+    BitTreeNode current = this.root;
+    for(int i = 0; i < stringSize; i++) {
+      if (bits.charAt(i) == '0') {
+        if (current.left == null) {
+          BitTreeNode left = new BitTreeNode();
+          current.left = left;
+        } // if
+        current = current.left;
+      } else {
+        if (current.right == null) {
+          BitTreeNode right = new BitTreeNode();
+          current.right = right;
+        } // if
+        current = current.right;
+      } // if
+    } // for
+    current.value = value;
   } // set(String, String)
 
   /**
-   *
+   * Gets the 
    */
   public String get(String bits) {
-    return "";  // STUB
+    if (bits.length() != stringSize) {
+      throw new IndexOutOfBoundsException();
+    } //if
+
+    BitTreeNode current = this.root;
+    for(int i = 0; i < stringSize; i++) {
+      if (bits.charAt(i) == '0') {
+        if (current.left == null) {
+          throw new IndexOutOfBoundsException();
+        } // if
+        current = current.left;
+      } else {
+        if (current.right == null) {
+          throw new IndexOutOfBoundsException();
+        } // if
+        current = current.right;
+      } // if
+    } // for
+    
+    if (current.value == null) {
+      throw new IndexOutOfBoundsException();
+    } else {
+      return current.value;
+    } // if
   } // get(String, String)
 
   /**
