c59d24a4de6c1bbf50f52f22c1c5b2482e78f137
Ryanalys <ryanalys@grinnell.edu>
2024-12-08 22:03:03 -0600

    fixed checkstyle errors

--- src/main/java/edu/grinnell/csc207/util/BitTreeNode.java
+++ src/main/java/edu/grinnell/csc207/util/BitTreeNode.java
@@ -1,20 +1,29 @@
 package edu.grinnell.csc207.util;
 
+
+/**
+ * A single node of the BitTree.
+ *
+ * Author: Alyssa Ryan
+ */
 public class BitTreeNode {
   /**
-   * The value of this node, if it is a leaf
+   * The value of this node, if it is a leaf.
    */
   String value = null;
 
   /**
-   * The left child of this node, if it is an interior node
+   * The left child of this node, if it is an interior node.
    */
   BitTreeNode left = null;
   /**
-   * The right child of this node, if it is an interior node
+   * The right child of this node, if it is an interior node.
    */
   BitTreeNode right = null;
 
+  /**
+   * The constructor for BitTreeNode.
+   */
   public BitTreeNode() {
     this.value = null;
     this.left = null;
