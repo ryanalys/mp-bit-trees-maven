10ebe3750b4c7039f5a166fe174cee0d264472cf
Ryanalys <ryanalys@grinnell.edu>
2024-12-08 22:03:54 -0600

    more checkstyle stuff

--- src/main/java/edu/grinnell/csc207/experiments/BATableExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/BATableExperiments.java
@@ -10,7 +10,7 @@ import java.io.PrintWriter;
 /**
  * Some experiments with the Braille-Ascii Tables.
  *
- * @author Your Name Here
+ * @author Alyssa Ryan
  * @author Samuel A. Rebelsky
  */
 public class BATableExperiments {
